54012d2b48a0a38e4bcc5e3777e275e23b00c81a
landermo <lmontgomery@boomtownroi.com>
2017-01-12 17:15:45 -0500

    Partway through.

---
+++ bob.rb
@@ -0,0 +1,27 @@
+# Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+# Bob answers 'Sure.' if you ask him a question.
+# He answers 'Whoa, chill out!' if you yell at him.
+# He says 'Fine. Be that way!' if you address him without actually saying anything.
+# He answers 'Whatever.' to anything else.
+
+
+class Bob
+  def initialize
+
+  end
+  def hey(remark)
+    response = ['Sure.','Whoa, chill out!','Fine. Be that way!', 'Whatever.']
+
+    if remark.upcase == remark
+      response[1]
+
+    elsif remark.end_with? '?'
+      response[0]
+    elsif
+    remark.empty?
+      response[2]
+    else
+      response[3]
+    end
+  end
+end
